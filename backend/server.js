96fbfa78891d30472b3e85360795fb3e05742546
Mohsin Pannu <mohsin.pannu170@gmail.com>
2023-10-10 15:39:03 +0500

    created database models and connected with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 dotenv.config()
 const app = express()
 const port = process.env.PORT
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
