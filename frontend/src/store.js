f2ce6e8f9744466c76dc63ea86164133517d5331
Mohsin Pannu <mohsin.pannu170@gmail.com>
2023-10-11 05:59:44 +0500

    configured redux toolkit and created store.

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+// STORE - Entry Point for the redux
+
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
+})
+
+export default store
