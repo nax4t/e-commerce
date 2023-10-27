9836d3be647714c23f390b216656bca186a0a418
Mohsin Pannu <mohsin.pannu170@gmail.com>
2023-10-27 22:45:25 +0500

    completed :)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import {
 } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import {HelmetProvider} from 'react-helmet-async'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
@@ -69,9 +70,11 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 )
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,6 +7,7 @@ import Message from '../components/Message'
 import Paginate from '../components/Paginate'
 import { Link } from 'react-router-dom'
 import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
 
 const HomeScreen = () => {
   const {keyword, pageNumber} = useParams()
@@ -30,6 +31,7 @@ const HomeScreen = () => {
         </Message>
       ) : (
         <>
+        <Meta />
         <h1>Latest Products</h1>
         <Row>
           {data.products.map((product) => (
